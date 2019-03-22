303edd259e142545e1c1c7191b87b1d03acea800
Karen Shea <shea.karene@gmail.com>
2019-03-21 21:13:15 -0400

    changed error handling functions

--- app.js
+++ app.js
@@ -28,19 +28,32 @@ app.use('/', indexRouter);
 app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// app.use(function(req, res, next) {
+//   const error = new Error('Page Not Found - 404');
+// 	error.status = 404;
+// 	next(error);
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+app.use((req, res, next) => {
+	const error = new Error('Page Not Found - 404');
+	error.status = 404;
+	next(error);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use((error, req, res, next) => {
+	res.locals.error = error;
+	res.render('page-not-found', {error}); //error.pug pass in error
+})
 
 module.exports = app;
